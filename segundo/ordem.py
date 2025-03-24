0ebaab930f403f68cce2d21557aa2e3e61bc8137
Douglas Araujo <douglasdab10@gmail.com>
2025-03-24 09:53:25 -0300

    Update ordem.py

--- segundo/ordem.py
+++ segundo/ordem.py
@@ -1,8 +1,11 @@
-#lista de tupla ordenada
 print("-"*50)
+
 lista_nome = [("Douglas", 18), ("Samuel", 22), ("Karine", 22), ("João", 25), ("Joy", 20), ("Kleber", 28), ("Carol", 26)]
-    
-lista_ordenada = sorted(lista_nome, key=lambda x: x[1])
-  
-print(lista_ordenada)
+
+def ordenar_lista(lista_nome):
+    lista_ordenada = sorted(lista_nome, key=lambda x: x[1])
+    return lista_ordenada
+
+print(ordenar_lista(lista_nome))
+
 print("-"*50)
